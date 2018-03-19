bca42567b8a7418a4235c6365cec3ed2a04b4f20
André Rosa <andrefromportugal@gmail.com>
2018-03-19 23:55:32 +0100

    Update predictor.sh

--- scripts/predictor.sh
+++ scripts/predictor.sh
@@ -7,31 +7,27 @@ echo ""
 echo "TopoPRED will now prepare your test file."
 echo ""
 
-#python data_prep.py
+python data_prep.py
 
 cd ../bin/
 
-#if [ ! -f .fasta.gz ]; then
-#    wget ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref90/uniref90.fasta.gz
-#gunzip *.gz
-#fi
+if [ ! -f .fasta.gz ]; then
+    wget ftp://ftp.uniprot.org/pub/databases/uniprot/uniref/uniref90/uniref90.fasta.gz
+gunzip *.gz
+fi
 
-#if [ ! -f *.phr ]; then
- #   makeblastdb -in uniref50.fasta -dbtype "prot" -out nr
-    #makeblastdb -in uniref90 BLA BLA BLA
-#fi
+if [ ! -f *.phr ]; then
+    makeblastdb -in uniref90.fasta -dbtype "prot" -out nr
+fi
 
 for file in *.fa; do
     if [ ! -f $file.pssm ]; then
-#	echo "Running PSI-BLAST on $file at $(date). Please wait ..."
-#	echo ""
-	psiblast -query $file -evalue 10 -db /mnt/c/KB8024/bin/nr -num_iterations 2 -out ../data/$file.psiblast -out_ascii_pssm ../data/$file.pssm
-#	echo ""
-#	echo "PSI-BLAST on $file terminated at $(date)."
+	echo "Running PSI-BLAST on $file at $(date). Please wait ..."
+	echo ""
+	psiblast -query $file -evalue 0.005 -db /mnt/c/KB8024/bin/nr -num_iterations 3 -out ../data/$file.psiblast -out_ascii_pssm ../data/$file.pssm
+	echo ""
+	echo "PSI-BLAST on $file terminated at $(date)."
     fi
 done
 
-#Building a new DB, current time: 03/17/2018 13:13:34
-#New DB name:   /mnt/c/KB8024/bin/nr
-#New DB title:  uniref50.fasta
-#Sequence type: Protein
+python SM_SVM_PSSM.py
